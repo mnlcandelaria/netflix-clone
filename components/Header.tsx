f716d8dd817c51403547dc5338648e8ea5b33338
Manly Candelaria <mnl.candelaria@gmail.com>
2022-08-02 00:59:37 +0800

    build complete

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
 import useAuth from '../hooks/useAuth'
+import BasicMenu from './BasicMenu'
 
 const Header = () => {
   const [isScrolled, setIsScrolled] = useState(false)
@@ -32,6 +33,9 @@ const Header = () => {
           height={100}
           className="current-pointer object-contain"
         />
+
+        <BasicMenu />
+
         <ul className="hidden space-x-4 md:flex">
           <li className="headerLink">Home</li>
           <li className="headerLink">TV Shows</li>
@@ -44,14 +48,13 @@ const Header = () => {
         <SearchIcon className="hidden sm:inline w-6 h-6" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
-        {/*<Link href="/account"> */}
-        <img
-          onClick={logout}
-          src="https://rb.gy/g1pwyx"
-          alt=""
-          className="cursor-pointer rounded"
-        />
-        {/*</Link>*/}
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   )
